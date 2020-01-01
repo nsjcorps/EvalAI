d6748087e96153c98918f8b098bb22effb29fdcc
jayy <35180217+nsjcorps@users.noreply.github.com>
2020-01-01 19:46:04 +0100

    spike test

--- scripts/deployment/docker_push.sh
+++ scripts/deployment/docker_push.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag local-image:evalai-image evalai-image:evalai-image
-docker push ndujay/evalai-image:evalai-image
+docker images
