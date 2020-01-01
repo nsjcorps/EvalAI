25996200d103aef1817657d13acb675b14985c6d
jayy <35180217+nsjcorps@users.noreply.github.com>
2020-01-01 21:03:02 +0100

    spike test

--- scripts/deployment/docker_push.sh
+++ scripts/deployment/docker_push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+export COMMIT_ID=$(git rev-parse HEAD)
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker pull ubuntu
 docker pull ndujay/evalai-image
 docker images
+docker build -t ndujay/evalai-image:${COMMIT_ID} .
 docker push nsjcorps/test-lamp-server
