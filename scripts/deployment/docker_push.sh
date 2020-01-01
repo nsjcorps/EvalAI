b38999997046d8f5b228af191529e219ed8fc7ed
jayy <35180217+nsjcorps@users.noreply.github.com>
2020-01-01 20:23:04 +0100

    spike test

--- scripts/deployment/docker_push.sh
+++ scripts/deployment/docker_push.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker pull ubuntu
-docker run --name evalai-image -it ubuntu:latest bash
-apt-get install lamp-server
+docker run --name test-lamp-server -it ubuntu:latest bash
+apt-get update
 exit
 docker ps -a
+docker commit -m "Added LAMP Server" -a "NAME" test-lamp-server nsjcorps/test-lamp-server:latest
+docker push nsjcorps/test-lamp-server
