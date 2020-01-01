169fae28970fdb94291cb58ba082032966091d5d
jayy <35180217+nsjcorps@users.noreply.github.com>
2020-01-01 21:42:43 +0100

    spike test

--- scripts/deployment/docker_push.sh
+++ scripts/deployment/docker_push.sh
@@ -3,6 +3,12 @@ export COMMIT_ID=$(git rev-parse HEAD)
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker images
+aws configure set default.region us-east-1
+eval $(aws ecr get-login --no-include-email)
+echo "Pulling ssl certificates and nginx configuration..."
+aws s3 cp s3://cloudcv-secrets/evalai/staging/ssl/ ./ssl/ --recursive
+aws s3 cp s3://cloudcv-secrets/evalai/staging/nginx_staging.conf ./docker/prod/nodejs/nginx_staging.conf
+echo "Pulled ssl certificates and nginx configuration successfully"
 docker-compose -f docker-compose-staging.yml build
 docker-compose -f docker-compose-staging.yml push
 
