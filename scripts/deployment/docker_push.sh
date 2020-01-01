000b7c7798791faedf2710cd44f9c45a3f12a438
jayy <35180217+nsjcorps@users.noreply.github.com>
2020-01-01 21:17:05 +0100

    spike test

--- scripts/deployment/docker_push.sh
+++ scripts/deployment/docker_push.sh
@@ -2,8 +2,17 @@
 export COMMIT_ID=$(git rev-parse HEAD)
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker pull ubuntu
 docker images
-docker build -t ndujay/evalai-image:${COMMIT_ID} .
-docker tag ndujay/evalai-image:${COMMIT_ID} ndujay/evalai-image:latest
-docker push nsjcorps/evalai-image
+docker-compose -f docker-compose-$1.yml build
+docker-compose -f docker-compose-$1.yml push
+
+# Get already built docker images
+images=$(cat docker-compose-$1.yml | grep 'image: ' | cut -d':' -f 2 | tr -d '"')
+
+# Tag & push images with latest tag
+for image in $images
+do
+    eval image=${image}
+    docker tag ${image}:${COMMIT_ID} ${image}:latest
+    docker push ${image}:latest
+done
